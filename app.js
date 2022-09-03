8acfc147fcc810b1ab10dd119c8582fa4c48110d
Yhunn <jybrant.efren@gmail.com>
2022-09-03 14:35:48 -0600

    soporte de vista y rendering html

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const port = 3000;
-
 const app = express();
-
 const db = require('./db');
 
 app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+app.set("view engine", "ejs");
+
+app.get('/', (req,res) => {
+    res.render("index.ejs");
+});
 
 const usersRoute = require("./routes/users_route");
 app.use("/users", usersRoute);
