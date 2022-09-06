9e2718ec390103559974432051c4feb1febbf449
Yhunn <jybrant.efren@gmail.com>
2022-09-05 18:28:53 -0600

    echo po list into frontend in load

--- routes/po_query_route.js
+++ routes/po_query_route.js
@@ -3,7 +3,19 @@ const router = express.Router();
 const db = require('../db');
 
 router.get('/', (req,res)=>{
-    res.render("po_query.ejs");
+    var poToDeliverList = [];
+    try {
+        db.query(`SELECT MIN("id") AS "id", "identifierOrder" FROM "po_toDeliver_provisional" GROUP BY "identifierOrder" ORDER BY "id" ASC`)
+            .then(rows => {
+                rows.forEach(row =>{
+                    poToDeliverList.push(row.identifierOrder);
+                });
+                res.render("po_query.ejs",{poList: poToDeliverList});
+            });
+    } catch (err) {
+        console.warn("Unable to insert into database");
+    }
+    
 });
 
 module.exports = router;
