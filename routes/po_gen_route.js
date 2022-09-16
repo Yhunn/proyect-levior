0d8e02accbc37d5a2e1a8fd99456d1c7fe161815
Yhunn <jybrant.efren@gmail.com>
2022-09-16 16:22:29 -0600

    sketch for fetching last po on create

--- routes/po_gen_route.js
+++ routes/po_gen_route.js
@@ -3,7 +3,39 @@ const router = express.Router();
 const db = require('../db');
 
 router.get('/', checkPermissionsForPOGenerator, (req, res) => {
-    res.render("po_generator.ejs");
+    const idUserOffice = req.user.office;
+    try {
+        //LOAD CURRENT OFFICE OFF USER DATA
+        db.any(`SELECT * FROM "office_locations" WHERE "id" = $1`,[idUserOffice])
+        .then(rows =>{
+            const officeData = []
+            rows.forEach(row =>{
+                officeData.push({
+                    id: row.id,
+                    officeName: row.office_name,
+                    abreviation: row.abreviation
+                });
+            });
+            //SEARCH FOR LAST PO GENERATED FROM LOCATION
+            /*  --
+                const poDate = new Date();
+                const poMonth = poDate.getMonth() +1;
+                const poYear = poDate.getFullYear();
+                const poIdDate = String(poYear).concat("-",poMonth);
+
+                DB QUERY SELECT MAX("monthlyNumeration") FROM "purchase_order" WHERE (OFFICE = idUserOffice.id
+                && PO_Period = poIdDate)
+                then(rows =>{
+                    const nextPO = Integer(rows[0].monthlyNumeration) + 1;
+                    render('pogenerator.ejs', {officeData, netPO})
+                });
+
+            */
+            res.render("po_generator.ejs",{ officeData: officeData[0] });
+        });
+    } catch (error) {
+        console.warn('Failed to load from database');
+    }
 });
 
 router.post('/save', (req, res) => {
